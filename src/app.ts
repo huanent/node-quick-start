4043f9b75f97006ec5edc5ca1a2fa50be650b1a3
huanent <huanent@hotmail.com>
2018-09-15 00:56:36 +0800

    新增静态网站托管

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,14 @@ import bodyParser from 'body-parser'
 
 const app = express();
 
+//body处理
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+//开启静态网站托管
+app.use(express.static("public"))
+app.use("/",(req,rsp)=>{
+    rsp.sendFile("public/index.html")
+})
 
 export default app
